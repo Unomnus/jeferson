0ef5f65f73e45c1a6f23fb04a03887ae0930c99a
Jeferson <kaipos.jef@gmail.com>
2026-09-02 09:08:58 -0300

    features: tangente

---
+++ Aula07.py
@@ -0,0 +1,8 @@
+print("==== SENOS, COSENOS E TANGENTES ====")
+
+seno = float(input('Seno: '))
+cosseno = float(input('Cosseno: '))
+tangente = seno / cosseno
+print(f'Tangente: {tangente:.2f}')
+
+print("==== FIM ====")
