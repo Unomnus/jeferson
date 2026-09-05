2ac97e5ae86fe5534d73e813cbdf3ccb788eb369
Jeferson <kaipos.jef@gmail.com>
2026-09-05 08:15:22 -0300

    features: new exercise

--- Aula08.py
+++ Aula08.py
@@ -1,1 +1,11 @@
+import random
 print('========== SORTEIO ==========')
+n1 = str(input('Digite o nome do primeiro aluno: '))
+n2 = str(input('Digite o nome do segundo aluno: '))
+n3 = str(input('Digite o nome do terceiro aluno: '))
+n4 = str(input('Digite o nome do quarto aluno: '))
+lista = [n1, n2, n3, n4]
+sorteio = random.choice(lista)
+print('O aluno sorteado foi: {}'.format(sorteio))
+
+print('========== FIM ==========')
